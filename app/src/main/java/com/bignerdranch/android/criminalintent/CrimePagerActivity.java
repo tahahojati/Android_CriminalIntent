3ab1406fd7a4070880b2a60e509b4ae10da04b6b
Taha <taha.hojati@gmail.com>
2017-10-17 14:38:39 -1000

    Added First and Last page buttons to the viewpager activity.  The buttons are disabled if user is already on first page or last page.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +19,7 @@ public class CrimePagerActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
     private UUID mCurrentCrimeId;
+    private Button mFirstCrimeButton, mLastCrimeButton;
     public static final String EXTRA_CRIME_ID =
             "com.bignerdranch.android.criminalintent.crime_id";
 
@@ -24,6 +27,8 @@ public class CrimePagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+        mFirstCrimeButton = (Button) findViewById(R.id.first_crime_button);
+        mLastCrimeButton = (Button) findViewById(R.id.last_crime_button);
         mCurrentCrimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
@@ -40,12 +45,52 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position +1 == mCrimes.size()){
+                    mFirstCrimeButton.setEnabled(true);
+                    mLastCrimeButton.setEnabled(false);
+                }
+                else if(position == 0){
+                    mFirstCrimeButton.setEnabled(false);
+                    mLastCrimeButton.setEnabled(true);
+                }
+                else{
+                    mFirstCrimeButton.setEnabled(true);
+                    mLastCrimeButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         for (int i = 0; i < mCrimes.size(); i++) {
             if (mCrimes.get(i).getId().equals(mCurrentCrimeId)) {
                 mViewPager.setCurrentItem(i);
                 break;
             }
         }
+
+        mFirstCrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        mLastCrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(mCrimes.size()-1);
+            }
+        });
     }
 
     static Intent newIntent(Context context, UUID crimeId){
